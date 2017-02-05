4e9f71c6ea5b7d252f4f7e3a5c36545256a10d91
Sam Fuchs <samcfuchs@gmail.com>
2017-02-05 18:04:11 -0500

    Add auto parameters

--- src/org/usfirst/frc/team236/robot/AutoMap.java
+++ src/org/usfirst/frc/team236/robot/AutoMap.java
@@ -1,8 +1,22 @@
 package org.usfirst.frc.team236.robot;
 
 public final class AutoMap {
+	// Straight gear delivery
+	public static final ProfileParameters straightGear = new ProfileParameters(77.5, 20, 2, 10);
 
-	public AutoMap() {
+	// Right gear delivery
+	public static final ProfileParameters rightGearLeg1 = new ProfileParameters(72, 20, 2, 10);
+	public static final double rightGearDegrees = 61.5;
+	public static final ProfileParameters rightGearLeg2 = new ProfileParameters(70, 20, 2, 10);
 
-	}
+	// Left gear delivery
+	public static final ProfileParameters leftGearLeg1 = new ProfileParameters(70.9, 30, 2, 10);
+	public static final double leftTurnDegreees = 60;
+	public static final ProfileParameters leftGearLeg2 = new ProfileParameters(69.5, 30, 2, 10);
+
+	// General Parameters
+	public static final DriveParameters leftParams = new DriveParameters(RobotMap.Drive.kV_left, RobotMap.Drive.kA,
+			RobotMap.Drive.kP);
+	public static final DriveParameters rightParams = new DriveParameters(RobotMap.Drive.kV_right, RobotMap.Drive.kA,
+			RobotMap.Drive.kP);
 }
