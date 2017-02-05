4d653c37570c7e1b65d5f5f9088cd341cdc2c41b
Sam Fuchs <samcfuchs@gmail.com>
2017-02-05 18:04:49 -0500

    Add control map file

---
+++ src/org/usfirst/frc/team236/robot/ControlMap.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team236.robot;
+
+public class ControlMap {
+	public static final int PORT_LEFT = 0;
+	public static final int PORT_RIGHT = 1;
+	public static final int PORT_CONTROLLER = 2;
+
+	public static class Left {
+
+	}
+
+	public static class Right {
+
+	}
+
+	public static class Controller {
+
+	}
+}
