664a36feabd7ff2ac61ad7a0b4f76de2bb51e923
Sam Fuchs <samcfuchs@gmail.com>
2017-02-06 01:09:22 -0500

    Add motor to climber

--- src/org/usfirst/frc/team236/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team236/robot/subsystems/Climber.java
@@ -1,9 +1,20 @@
 package org.usfirst.frc.team236.robot.subsystems;
 
+import org.usfirst.frc.team236.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedController;
+
 public class Climber {
 
+	private SpeedController motor;
+
 	public Climber() {
-		// TODO Auto-generated constructor stub
+		motor = new Spark(RobotMap.Climber.PWM_CLIMBER);
+	}
+
+	public void setSpeed(double speed) {
+		motor.set(speed);
 	}
 
 }
