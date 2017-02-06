f69fcafe3b437e0b4c5d2bea31d16b2c97d5dd8c
Sam Fuchs <samcfuchs@gmail.com>
2017-02-06 01:08:51 -0500

    Add joysticks to OI

--- src/org/usfirst/frc/team236/robot/OI.java
+++ src/org/usfirst/frc/team236/robot/OI.java
@@ -1,8 +1,21 @@
 package org.usfirst.frc.team236.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	public Joystick left, right;
+	public Joystick controller;
+
+	public OI() {
+		// Joysticks + controller
+		left = new Joystick(ControlMap.PORT_LEFT);
+		right = new Joystick(ControlMap.PORT_RIGHT);
+		controller = new Joystick(ControlMap.PORT_CONTROLLER);
+
+		// Add buttons below
+	}
 }
