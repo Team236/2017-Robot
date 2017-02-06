a002be50e3ea15ce612b2f52e1be4e2dfe3a051d
Sam Fuchs <samcfuchs@gmail.com>
2017-02-06 10:43:38 -0500

    Add empty raise and lower commands

---
+++ src/org/usfirst/frc/team236/robot/commands/garage/Raise.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team236.robot.commands.garage;
+
+import org.usfirst.frc.team236.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.TimedCommand;
+
+/**
+ *
+ */
+public class Raise extends TimedCommand {
+
+	public Raise(double timeout) {
+		super(timeout);
+		requires(Robot.garageDoor);
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+	}
+
+	// Called once after timeout
+	@Override
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
+}
