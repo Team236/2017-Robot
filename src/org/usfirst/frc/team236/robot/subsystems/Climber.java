aacbbaf2bdadb8ef777f3d97b348ae361c07c378
Sam Fuchs <samcfuchs@gmail.com>
2017-02-18 16:18:33 -0500

    Add limit switches to climber

--- src/org/usfirst/frc/team236/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team236/robot/subsystems/Climber.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team236.robot.subsystems;
 
 import org.usfirst.frc.team236.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -10,16 +11,30 @@ public class Climber extends Subsystem {
 
 	private SpeedController motor;
 
+	private DigitalInput limit;
+	
 	public Climber() {
 		motor = new Spark(RobotMap.Climber.PWM_CLIMBER);
+		motor.setInverted(RobotMap.Climber.INV_CLIMBER);
+		
+		limit = new DigitalInput(RobotMap.Climber.DIO_LIMIT);
 	}
 
 	public void setSpeed(double speed) {
-		motor.set(speed);
+		if (!isTop()) {
+			motor.set(speed);
+		} else {
+			stop();
+		}
+	}
+	
+	public boolean isTop() {
+		System.out.println(!limit.get());
+		return !limit.get();
 	}
 
 	public void stop() {
-		setSpeed(0);
+		motor.set(0);
 	}
 
 	@Override
