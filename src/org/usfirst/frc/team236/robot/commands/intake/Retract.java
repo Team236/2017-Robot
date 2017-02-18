a29a840e04f08b00622576711149649a708f008a
Sam Fuchs <samcfuchs@gmail.com>
2017-02-18 14:35:02 -0500

    Add Retract intake command

---
+++ src/org/usfirst/frc/team236/robot/commands/intake/Retract.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team236.robot.commands.intake;
+
+import org.usfirst.frc.team236.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class Retract extends Command {
+
+	public Retract() {
+		requires(Robot.intake);
+	}
+
+	@Override
+	protected void initialize() {
+		Robot.intake.stop();
+	}
+
+	@Override
+	protected void execute() {
+		Robot.intake.retract();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		Robot.intake.stop();
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+	}
+}
