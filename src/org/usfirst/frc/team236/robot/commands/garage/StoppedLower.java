c04881338c39370e1c3ed7ca1c0bb522cbe8ee07
Sam Fuchs <samcfuchs@gmail.com>
2017-02-19 20:08:25 -0500

    Add StoppedLower command

---
+++ src/org/usfirst/frc/team236/robot/commands/garage/StoppedLower.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team236.robot.commands.garage;
+
+import org.usfirst.frc.team236.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class StoppedLower extends Command {
+
+	private int i = 0;
+
+	public StoppedLower() {
+		requires(Robot.garage);
+		requires(Robot.tank);
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		Robot.garage.grasp();
+		Robot.garage.lightOn();
+		Robot.tank.stop();
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		if (i / 50 > 0.5) {
+			Robot.garage.lower();
+		}
+		i++;
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return (i / 50 > 1);
+	}
+
+	// Called once after timeout
+	@Override
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+		end();
+	}
+}
