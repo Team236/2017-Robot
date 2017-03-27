1ed4290a1b5a871210d9f509bc2450be63f3d14f
Sam Fuchs <samcfuchs@gmail.com>
2017-03-27 17:51:23 -0400

    Fix null pointer error in Joystick POV class

--- src/org/usfirst/frc/team236/robot/triggers/JoystickPOV.java
+++ src/org/usfirst/frc/team236/robot/triggers/JoystickPOV.java
@@ -16,11 +16,13 @@ public class JoystickPOV extends Button {
 	public JoystickPOV(Joystick _stick, int _pov, Direction _dir) {
 		this.stick = _stick;
 		this.pov = _pov;
+		this.dir = _dir;
 	}
 
 	public JoystickPOV(Joystick _stick, Direction _dir) {
 		this.stick = _stick;
 		this.pov = 0;
+		this.dir = _dir;
 	}
 
 	@Override
