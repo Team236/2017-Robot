9d66d384da6e4b80a43a2b1324d056db396fd8f9
Sam Fuchs <samcfuchs@gmail.com>
2017-03-30 16:50:18 -0400

    Add DriveStraight command

---
+++ src/org/usfirst/frc/team236/robot/commands/auto/DriveStraight.java
@@ -0,0 +1,89 @@
+package org.usfirst.frc.team236.robot.commands.auto;
+
+import org.usfirst.frc.team236.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveStraight extends Command {
+
+	private double kP = 0.5;
+	private double speed = -0.5;
+	private double dist;
+
+	// Collision detection
+	double lastAccel, currAccel;
+	double jerk;
+	public static final double collisionThreshold = 1; //TODO Set collision threshold
+
+	int i = 0;
+
+	/**
+	 * @param _kP The proportional feedback constant
+	 * @param _dist The distance to drive
+	 */
+	public DriveStraight(double _kP, double _dist, double _speed) {
+		this.kP = _kP;
+		this.dist = _dist;
+		this.speed = _speed;
+	}
+
+	public DriveStraight(double _dist) {
+		this.dist = _dist;
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		Robot.tank.zeroEncoders();
+		Robot.tank.navx.reset();
+		i = 0;
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		i++;
+
+		double ang = Robot.tank.navx.getAngleAdjustment();
+
+		double leftSpeed = speed;
+		double rightSpeed = speed;
+
+		leftSpeed += ang * kP;
+		rightSpeed -= ang * kP;
+
+		Robot.tank.setSpeeds(leftSpeed, rightSpeed);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		// Stop command if both encoders have gone the distance
+		if (Robot.tank.getLeftEncoder().getDistance() > dist && Robot.tank.getRightEncoder().getDistance() > dist) {
+			return true;
+		}
+
+		currAccel = Robot.tank.navx.getWorldLinearAccelY(); // TODO figure out which axis is forward
+		jerk = currAccel - lastAccel;
+		lastAccel = currAccel;
+
+		if (Math.abs(jerk) > collisionThreshold) {
+			return true;
+		}
+
+		return (i / 50 > 1.5);
+	}
+
+	@Override
+	protected void end() {
+		Robot.tank.stop();
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+	}
+}
