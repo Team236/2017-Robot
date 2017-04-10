7fbff976743ed003290b455605d17b5823cea981
Sam Fuchs <samcfuchs@gmail.com>
2017-04-10 11:08:41 -0400

    Speed up command execution in gyro auto

--- src/org/usfirst/frc/team236/robot/commands/auto/CenterWithGyro.java
+++ src/org/usfirst/frc/team236/robot/commands/auto/CenterWithGyro.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team236.robot.commands.auto;
 
 import org.usfirst.frc.team236.robot.Robot;
-import org.usfirst.frc.team236.robot.commands.Wait;
 import org.usfirst.frc.team236.robot.commands.drive.DriveStraight;
 import org.usfirst.frc.team236.robot.commands.garage.Grasp;
 import org.usfirst.frc.team236.robot.commands.garage.Lower;
@@ -14,11 +13,10 @@ import ticktank.commands.ShiftUp;
  */
 public class CenterWithGyro extends CommandGroup {
 
-    public CenterWithGyro() {
-    	addSequential(new ShiftUp(Robot.tank));
-    	addSequential(new Grasp());
-    	addSequential(new Lower());
-    	addSequential(new Wait(1));
+	public CenterWithGyro() {
+		addParallel(new ShiftUp(Robot.tank));
+		addParallel(new Grasp());
+		addSequential(new Lower());
 		addSequential(new DriveStraight(80));
-    }
+	}
 }
